da10a53dc7cca54aa56449990b7bd34488e0e2e2
Reika <wnxy1533967@gmail.com>
2023-09-22 16:54:50 +0800

    completed face recognition part using carifai api

--- src/App.js
+++ src/App.js
@@ -6,11 +6,94 @@ import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import ParticlesBg from 'particles-bg'
+// import Clarifai from 'clarifai';
+
+// const app = new Clarifai.App({
+//   apiKey: '6982e30b2c0042db811ef8657c3c312b'
+//  });
+
+ 
+const clarifaiRequestOption = (imageUrl) => {
+  const PAT = '6982e30b2c0042db811ef8657c3c312b';
+  const USER_ID = 'c1z3v00fdmjx';       
+  const APP_ID = 'my-first-application-qqjot';
+  const MODEL_ID = 'face-detection';
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                }
+            }
+        }
+    ]
+  });
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+    },
+    body: raw
+  };
+
+  return requestOptions;
+}
+
+// fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", clarifaiRequestOption(this.state.input))
+//     .then(response => response.text())
+//     .then(result => console.log(result))
+//     .catch(error => console.log('error', error));
 
 class App extends Component {
+  
   constructor() {
     super()
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+    }
   }
+  
+  calculateFaceBox = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box)
+    this.setState({box: box})
+  }
+
+  onInputChange = (e) => {
+    this.setState({input: e.target.value})
+  }      
+  
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+    fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", clarifaiRequestOption(this.state.input))
+      .then(response => response.json())
+      .then(result => this.displayFaceBox(this.calculateFaceBox(result)))
+      .catch(error => console.log('error', error));
+
+  }
+  
   render() {
     return (
       <div className='App'>
@@ -18,8 +101,8 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        <FaceRecognition />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     )
   }
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './ImageLinkForm.css'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <>
             <p className="center text-xl font-medium">
@@ -9,8 +9,11 @@ const ImageLinkForm = () => {
             </p>
             <div className="center items-center">
                 <div className="p-8 rounded-lg shadow-lg center ImageForm">
-                    <input className="text-xl p-1 w-8/12 justify-center " type="text" />
-                    <button className="text-lg p-1 w-4/12 bg-green-200 transition ease-in-out hover:scale-110 duration-200 rounded-sm border-2 border-black">Detect</button>
+                    <input className="text-xl p-1 w-8/12 justify-center " type="text" onChange={ onInputChange }/>
+                    <button className="text-lg p-1 w-4/12 bg-green-200 transition ease-in-out 
+                    hover:scale-110 duration-200 rounded-sm border-2 border-black" onClick={ onButtonSubmit }>
+                        Detect
+                    </button>
                 </div>
                 
             </div>
